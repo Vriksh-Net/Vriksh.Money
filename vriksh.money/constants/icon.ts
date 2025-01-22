ceece6ecf4f73a35d2eb6589cf82ff55571709b6
ritikdevelop <hrithikrana216@gmail.com>
2025-01-22 14:06:05 +0530

    feat(icon.ts): icon.ts

---
+++ vriksh.money/constants/icon.ts
@@ -0,0 +1,22 @@
+import home from "../assets/icons/home.png";
+import profile from "../assets/icons/profile.png";
+import leftArrow from "../assets/icons/left-arrow.png";
+import rightArrow from "../assets/icons/right-arrow.png";
+import logout from "../assets/icons/logout.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+import profit from "../assets/icons/profit.png";
+import startup from "../assets/icons/startup.png";
+
+
+export default {
+  home,
+  profile,
+  leftArrow,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+  profit,
+  startup,
+};
